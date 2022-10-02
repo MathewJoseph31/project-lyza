9a3e1eb144b7bdd7f7d9aba11e57da30a80c689a
mathew joseph <mathew_joseph31@yahoo.com>
2022-10-02 09:24:04 -0400

    modal feature

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Modal from "..";
+
+afterEach(cleanup);
+
+const currentPhoto = {
+  name: "Park bench",
+  category: "landscape",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+  index: 1,
+};
+
+const mockToggleModal = jest.fn();
+
+describe("Modal component", () => {
+  // First Test
+  it("renders", () => {
+    render(<Modal currentPhoto={currentPhoto} onClose={mockToggleModal} />);
+  });
+
+  //snapshot test
+  it("matches snapshot", () => {
+    const { asFragment } = render(
+      <Modal currentPhoto={currentPhoto} onClose={mockToggleModal} />
+    );
+    // assert value comparison
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe("Click Event", () => {
+  it("calls onClose handler", () => {
+    // Arrange: Render Modal
+    const { getByText } = render(
+      <Modal onClose={mockToggleModal} currentPhoto={currentPhoto} />
+    );
+    // Act: Simulate click event
+    fireEvent.click(getByText("Close this modal"));
+    // Assert: Expected matcher
+    expect(mockToggleModal).toHaveBeenCalledTimes(1);
+  });
+});
